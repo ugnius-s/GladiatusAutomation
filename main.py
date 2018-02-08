a78ba18ad243bace27e7342de3dd8bcd58b3b157
Ugnius Skučas <ugnius.skucas@gmail.com>
2018-02-09 00:05:30 +0200

    More renaming

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from functions import *
 from objects import *
 
 USER = __import__(sys.argv[1])
-JOB_STABLES = __import__('script_stables')
+SCRIPT_STABLES = __import__('script_stables')
 SCRIPT_EXPEDITIONS = __import__('script_expeditions')
 
 # Create client driver;
@@ -21,6 +21,6 @@ log_in(client,USER,"First time log in")
 # 0 - 99 Job scripts;
 # 100 - 199 Expedition scripts;
 
-JOB_STABLES.loop(client, USER), # Work in stables
+SCRIPT_STABLES.loop(client, USER) # Work in stables
 #SCRIPT_EXPEDITIONS.loop(client, USER, 5, 4, 40) # Expeditions
 
