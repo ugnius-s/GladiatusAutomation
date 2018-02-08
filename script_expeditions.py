67469e368d97f923eb95e2214808eec1e865bd4d
Ugnius Skučas <ugnius.skucas@gmail.com>
2018-02-09 01:33:44 +0200

    Check for notifications before doing expedition

--- script_expeditions.py
+++ script_expeditions.py
@@ -20,19 +20,26 @@ def loop(client, user, place_selection, enemy_selection, max_hp, exit_on_zero_po
 
     if check_hp(client, max_hp):
       try:
+        check_notifications(client)
         menu_link = client.find_element_by_css_selector("#cooldown_bar_expedition > a:nth-child(3)")
         menu_link.click();
+
       except NoSuchElementException:
         puts("Can't access expeditions menu, maybe disconnected?")
     
       try:
         puts("Selected {0} place and {1} enemy".format(str(place_selection), str(enemy_selection)))
+        check_notifications(client)
         place_link = client.find_element_by_css_selector("#submenu2 > a:nth-child({0})".format(str(place_selection)))
         place_link.click();
+
       except (NoSuchElementException, ElementNotVisibleException):
         puts("Cannot select place {0}".format(str(place_selection)))
       time.sleep(1)
 
+      puts("Getting cooldowns")
+      check_notifications(client)
+	 
       cooldown_bar_text = client.find_element_by_css_selector("#cooldown_bar_text_expedition")	
       cooldown_indicator = client.find_elements_by_class_name("expedition_cooldown_reduce")
 	
@@ -42,6 +49,7 @@ def loop(client, user, place_selection, enemy_selection, max_hp, exit_on_zero_po
         wait_time = nums[0] * 3600 + nums[1] * 60 + nums[2]
       else:
         puts("Starting expedition")
+        check_notifications(client)
         enemy_link = client.find_element_by_css_selector(
           "div.expedition_box:nth-child({0}) > div:nth-child(2) > button:nth-child(1)".format(str(enemy_selection)))
         enemy_link.click()
@@ -52,4 +60,5 @@ def loop(client, user, place_selection, enemy_selection, max_hp, exit_on_zero_po
       if (eaten == False): # no food
         wait_time = 30 * 60
 
+    check_notifications(client)
     delay(wait_time)
